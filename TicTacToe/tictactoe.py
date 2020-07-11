e9c82889e9ff487ce370c6cb547ba4cc9e66ac96
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-07-10 22:00:15 -0400

    Update tictactoe.py

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -2,10 +2,8 @@ def print_board(b):
     for i in range(0, 9, 3):
         print(b[i:i+3])
 
-
 def is_valid_input(b, row, col):
     return 0 <= row and row <= 2 and 0 <= col and col <= 2 and b[row * 3 + col] == '.'
-
 
 def read_input(b):
     row, col = -1, -1
@@ -16,15 +14,12 @@ def read_input(b):
         col = int(input())
     return row * 3 + col
 
-
 def play_turn(b, player):
     print('Player ' + player + '\'s Turn')
     b[read_input(b)] = player
 
-
 def has_someone_won(b):
     return any(b[i] == b[j] and b[i] == b[k] and b[i] != '.' for i, j, k in wins)
-
 
 wins = [[0, 4, 8], [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6]]
