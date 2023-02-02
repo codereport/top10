06a3d47be9cf7e77ac0e1e3ad1e7ea6c1114390f
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2023-02-02 17:56:35 -0500

    Update max_gap.cpp

--- 06_max_gap/max_gap.cpp
+++ 06_max_gap/max_gap.cpp
@@ -1,8 +1,7 @@
 // Godbolt: https://godbolt.org/z/4Gab6reqT
 
 auto max_gap(std::vector<int> nums) {
-    std::ranges::sort(nums);
+    std::ranges::sort(nums, std::greater{});
     return std::ranges::max(nums
-        | reverse
         | adjacent_transform<2>(std::minus{}));
 }
