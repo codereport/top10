634fe17d32e7754965280198b093aba89360d363
Conor Hoekstra <codereport@outlook.com>
2023-04-19 16:14:05 -0400

    Update godbolt link

--- 02_mco/02_max_consecutive_ones.cpp
+++ 02_mco/02_max_consecutive_ones.cpp
@@ -1,5 +1,5 @@
 // Problem Link: https://leetcode.com/problems/max-consecutive-ones/
-// Godbolt: https://godbolt.org/z/r4Yf55fWf
+// Godbolt: https://godbolt.org/z/KGbza8Ka4
 
 auto mco(std::vector<int> lst) {
     return std::ranges::max(lst
