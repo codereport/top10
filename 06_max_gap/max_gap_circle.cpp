8d3f61290df9921e27d1e3bd42c64f30e8eaebc5
Conor Hoekstra <codereport@outlook.com>
2023-05-29 17:33:25 -0400

    Remove ufo and update with blackbird

--- 06_max_gap/max_gap_circle.cpp
+++ 06_max_gap/max_gap_circle.cpp
@@ -1,10 +1,10 @@
-// Godbolt Link: https://godbolt.org/z/4xz1c3j4v
+// Godbolt Link: https://godbolt.org/z/3K598jMMa
 
 using namespace std::views;
 
 auto max_gap(std::vector<int> nums) {
     std::ranges::sort(nums);
-    return nums                                          //
-           |> adjacent_transform<2>($, ufo::abs_diff{})  //
+    return nums                                    //
+           |> adjacent_transform<2>($, _c(_sub_))  //
            |> std::ranges::max($);
 }
