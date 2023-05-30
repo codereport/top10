dafc68f38a1596bc8edbdd731cd9d0fade67974e
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2023-05-30 09:44:04 -0400

    Update sushi_for_two.cpp

--- 05_sushi_for_two/sushi_for_two.cpp
+++ 05_sushi_for_two/sushi_for_two.cpp
@@ -1,9 +1,18 @@
 // Ranges Solution
-// Godbolt: https://godbolt.org/z/e5sn54Y86
+// Godbolt: https://godbolt.org/z/69Kh8baz3
 
+// pure standard c++
 auto sushi_for_two(std::vector<int> sushi) {
     return 2 * std::ranges::max(sushi
         | chunk_by(std::equal_to{})
+        | transform(std::ranges::distance)
+        | adjacent_transform<2>([](auto a, auto b) { return std::min(a, b); }));
+}
+
+// with blackbird
+auto sushi_for_two2(std::vector<int> sushi) {
+    return 2 * std::ranges::max(sushi
+        | chunk_by(_eq_)
         | transform(std::ranges::distance)
         | adjacent_transform<2>(_min_));
 }
