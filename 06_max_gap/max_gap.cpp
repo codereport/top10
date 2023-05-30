c58bac40a548b8e45090dad77b4d88ff93c149ed
Conor Hoekstra <codereport@outlook.com>
2023-05-30 15:34:37 -0400

    Additional max_gap c++

--- 06_max_gap/max_gap.cpp
+++ 06_max_gap/max_gap.cpp
@@ -1,9 +1,26 @@
-// Godbolt: https://godbolt.org/z/4Gab6reqT
+// Godbolt: https://godbolt.org/z/P43EhYcsj
 
+// Solution 1
 auto max_gap(std::vector<int> nums) {
-    std::ranges::sort(nums, std::greater{});
+    std::ranges::sort(nums);
     return std::ranges::max(  //
       nums                    //
       | reverse               //
       | adjacent_transform<2>(std::minus{}));
+}
+
+// Solution 2
+auto max_gap2(std::vector<int> nums) {
+    std::ranges::sort(nums);
+    return std::ranges::max(  //
+      nums                    //
+      | adjacent_transform<2>([](auto a, auto b) { return b - a; }));
+}
+
+// Solution 3 (with blackbird)
+auto max_gap3(std::vector<int> nums) {
+    std::ranges::sort(nums);
+    return std::ranges::max(  //
+      nums                    //
+      | adjacent_transform<2>(_c(_sub_)));
 }
